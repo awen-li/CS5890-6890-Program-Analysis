56127389b1e4bb65819a745a60271a3ab393f561
root <root@cs6890-PA.myguest.virtualbox.org>
2025-01-29 10:05:19 -0700

    add example for basic analysis

--- pa_template/myAnalysis/src/analysis.cpp
+++ pa_template/myAnalysis/src/analysis.cpp
@@ -1,18 +1,35 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
-
 #include "analysis.h"
 
-void analyzeModule(llvm::Module& module) {
-    for (auto& function : module) {
-        llvm::outs() << "Function: " << function.getName() << "\n";
+using namespace llvm;
 
-        for (auto& block : function) {
-            llvm::outs() << "  Basic block:\n";
-            for (auto& instruction : block) {
-                llvm::outs() << "    " << instruction << "\n";
-            }
+static inline void printFunctions (llvm::Module& module)
+{
+    outs() <<"@@printFunctions \n";
+
+    for (const auto &function : module) 
+    {
+        outs() << "Function: " << function.getName() << "\n";
+
+        const FunctionType *funcType = function.getFunctionType();
+        outs() << "\t>>Type: " << *funcType << "\n";
+
+        if (function.isDeclaration()) 
+        {
+            outs() << "\t>>Function is a declaration\n";
+        } 
+        else 
+        {
+            outs() << "\t>>Function is a definition\n";
         }
+
+        outs() <<"\n";
     }
+}
+
+void analyzeModule(llvm::Module& module) 
+{
+    printFunctions (module);
 }
