6a75aab284e93b182ec61d21a1df749639db4afc
awen-li <li.wen@wsu.edu>
2025-04-03 15:09:05 -0700

    add example for RE

--- lecture-materials/ReverseEngr/bench1/source/main.c
+++ lecture-materials/ReverseEngr/bench1/source/main.c
@@ -1,9 +1,17 @@
+#include<stdio.h>
+
 int g1 = 0;
 int g2;
 
+int direct_call(int val)
+{
+    return val++;
+}
+
 // hello world program
-int main(int argc, char* argv[]) 
+int main(int argc, char **argv)
 {
     printf("hello world!");
-    return 0;
+    return direct_call(argc);
 }
+
